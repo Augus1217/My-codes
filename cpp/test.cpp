052169b9a895215d7feda9d249620e9b6c404809
Augus1217 <imlindora@gmail.com>
2025-10-18 14:45:12 +0800

    20251018

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -2,6 +2,6 @@
 
 int main() {
     std::cout << "the quick brown fox jumps over the lazy dog" << std::endl;
-    std::cout << "嗨嗨!" << std::endl;
+    std::cout << "嗨！" << std::endl;
     return 0;
 }
